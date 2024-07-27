3a647c26d3a2d5fafaf48367234eb40d7715283d
ce310sar <cesarsaky0@gmail.com>
2024-07-27 18:43:40 -0500

    creación de boton para grafico de dispersión

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
     # crear un histograma
-    fig = px.histogram(df_car_data, x="odometer")
+    fig1 = px.histogram(df_car_data, x="odometer")
         
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
 
+disp_button = st.button('Construir grafico de dispersión')
+if disp_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig2 = px.scatter(df_car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
 # Agrega otro botón que, al hacer clic en él, construya un gráfico de dispersión plotly-express. 
 # Nuevamente, considera utilizar las funciones st.write() y st.plotly_chart().
