e4a02714601908df43cbb2fc3969ab53cd7ef783
ce310sar <cesarsaky0@gmail.com>
2024-07-27 18:56:27 -0500

    creación de headers

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import plotly_express as px
 
 df_car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma')
+
+st.header("Haz click para la creación de un histograma")
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -14,6 +16,7 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig1, use_container_width=True)
 
+st.header("Haz click para la creación de un gráfico de dispersión")
 disp_button = st.button('Construir grafico de dispersión')
 if disp_button: # al hacer clic en el botón
     # escribir un mensaje
